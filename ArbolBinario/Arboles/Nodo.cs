a08e55ba94882c8abaae4d5b67a1e3f9348162e2
JDavidDiaz <43283432+JDavidDiaz@users.noreply.github.com>
2018-11-08 23:09:45 -0800

    E4-1.DiazUriasJorgeDavid

---
+++ ArbolBinario/Arboles/Nodo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arboles
+{
+    class Nodo
+    {
+        public int info;
+        public Nodo izq, der;
+    }
+}
